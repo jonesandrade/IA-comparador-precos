5d555099e296978b48eff1a15e2ed4ffae374e83
jonesandrade <114091537+jonesandrade@users.noreply.github.com>
2026-05-03 12:05:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from scraper import buscar_precos
 from brain import analisar_melhor_opcao
+import os
 
 app = FastAPI()
 
+# Faz o FastAPI enxergar a pasta 'static'
+if os.path.exists("static"):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
 @app.get("/")
 def home():
-    return {"status": "IA de Preços Online"}
+    # Retorna o painel visual
+    return FileResponse("static/index.html")
 
 @app.get("/buscar")
 def pesquisar(nome_produto: str):
-    # 1. Coleta os dados brutos da internet
     dados_brutos = buscar_precos(nome_produto)
-    
-    # 2. IA analisa os dados e escolhe o melhor
     analise_final = analisar_melhor_opcao(dados_brutos)
-    
     return {"resultado": analise_final}
